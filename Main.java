99c45876c3c682789834e66d65866d0d8ccf28da
dlopezsibrian <dianalopezsibrian@gmail.com>
2026-05-29 20:52:40 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main(
+        2 - public static void main(String[] args)(
+            3
+            // using for loop because its from
+            here to there(start to finish)
+            //start at 1, go until 100 (inc), ++ (add 1)
+            5 -
+            for (int i - 1; 1 <= 100; i++)(
+                    //fizzbuzz for multiples of 3 and 5 (ex.15, 30, etc..)
+                    if ((1 * 3 == 0) & 8(1 % 5 -= 0))
+                        system.out.printIn("fizzbuzz"); Wif its not multiples of 3 and 5, print out fizz
+                    for multiples of 3(ex .3, 6, etc..) 18
+                    else if (1 % 3 == 0)
+                        System.out printIn("fizz"); I
+                    if its not multiples of 3 and 5, print out buzz
+                    for multiples of 5(ex .5, 10)
+                    else if (1 x5 == 0)
+                        System.out.println("buzz"); I /
+                    if its not multiples of 3 and 5, 3 or 5, print out the number itself(1, 2, 4, etc.)
+                    else System.out.println(1);
